c3fd1ee59f1e115752b38e5e72fc76bd2807a3cd
SimonDanisch <sdanisch@gmail.com>
2015-05-31 21:57:01 +0200

    removed println

--- src/display/postprocessing.jl
+++ src/display/postprocessing.jl
@@ -5,7 +5,6 @@ function postprocess(screen_texture, screen)
 		:resolution => lift(Vec2, screen.inputs[:framebuffer_size]),
 		:u_texture0 => screen_texture
 	), collect_for_gl(GLUVMesh2D(Rectangle(-1f0,-1f0, 2f0, 2f0))))
-	println(data[:vertices])
 	program = TemplateProgram(
 		File(shaderdir, "fxaa.vert"),
 		File(shaderdir, "fxaa.frag"),
