fc8eee99535fcd5dd6593c93b774140b6c93f2a1
SimonDanisch <sdanisch@gmail.com>
2015-06-01 22:13:54 +0200

    updated formatting

--- src/display/renderloop.jl
+++ src/display/renderloop.jl
@@ -2,20 +2,20 @@ const SELECTION         = Dict{Symbol, Input{Matrix{Vector2{Int}}}}()
 const SELECTION_QUERIES = Dict{Symbol, Rectangle{Int}}()
 
 function insert_selectionquery!(name::Symbol, value::Rectangle)
-  SELECTION_QUERIES[name] = value
-  SELECTION[name]         = Input(Vector2{Int}[]')
-  SELECTION[name]
+    SELECTION_QUERIES[name] = value
+    SELECTION[name]         = Input(Vector2{Int}[]')
+    SELECTION[name]
 end
 function insert_selectionquery!(name::Symbol, value::Signal{Rectangle{Int}})
-  lift(value) do v
+    lift(value) do v
     SELECTION_QUERIES[name] = v
-  end
-  SELECTION[name]         = Input(Array(Vector2{Int}, value.value.w, value.value.h))
-  SELECTION[name]
+    end
+    SELECTION[name]         = Input(Array(Vector2{Int}, value.value.w, value.value.h))
+    SELECTION[name]
 end
 function delete_selectionquery!(name::Symbol)
-  delete!(SELECTION_QUERIES, name)
-  delete!(SELECTION, name)
+    delete!(SELECTION_QUERIES, name)
+    delete!(SELECTION, name)
 end
 
 
@@ -28,17 +28,18 @@ windowhints = [
 ]
 
 const ROOT_SCREEN = createwindow("Romeo", 1920, 1280, windowhints=windowhints, debugging=false)
+
 insert_selectionquery!(:mouse_hover, lift(ROOT_SCREEN.inputs[:mouseposition]) do mpos
     Rectangle{Int}(round(Int, mpos[1]), round(Int, mpos[2]), 1,1)
 end)
 
 
 const FRAME_BUFFER_PARAMETERS = [
-  (GL_TEXTURE_WRAP_S,  GL_CLAMP_TO_EDGE),
-  (GL_TEXTURE_WRAP_T,  GL_CLAMP_TO_EDGE ),
+    (GL_TEXTURE_WRAP_S,  GL_CLAMP_TO_EDGE),
+    (GL_TEXTURE_WRAP_T,  GL_CLAMP_TO_EDGE ),
 
-  (GL_TEXTURE_MIN_FILTER, GL_NEAREST),
-  (GL_TEXTURE_MAG_FILTER, GL_NEAREST) 
+    (GL_TEXTURE_MIN_FILTER, GL_NEAREST),
+    (GL_TEXTURE_MAG_FILTER, GL_NEAREST) 
 ]
 
 global const RENDER_FRAMEBUFFER = glGenFramebuffers()
@@ -60,58 +61,61 @@ glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32, framebuffsize...)
 glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepthStencil[1])
 
 lift(ROOT_SCREEN.inputs[:framebuffer_size]) do window_size
-  if all(x->x>0, window_size)
-    resize_nocopy!(COLOR_BUFFER, tuple(window_size...))
-    resize_nocopy!(STENCIL_BUFFER, tuple(window_size...))
-    glBindRenderbuffer(GL_RENDERBUFFER, rboDepthStencil[1])
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32, (window_size)...)
-  end 
+    if all(x->x>0, window_size)
+        resize_nocopy!(COLOR_BUFFER, tuple(window_size...))
+        resize_nocopy!(STENCIL_BUFFER, tuple(window_size...))
+        glBindRenderbuffer(GL_RENDERBUFFER, rboDepthStencil[1])
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32, (window_size)...)
+    end 
 end
 
 
 postprocess_robj = postprocess(COLOR_BUFFER, ROOT_SCREEN)
 
 function renderloop()
-  global ROOT_SCREEN
-  while ROOT_SCREEN.inputs[:open].value
-    renderloop(ROOT_SCREEN)
-  end
-  GLFW.Terminate()
-end
-function renderloop(screen)
-  yield() 
-  glDisable(GL_SCISSOR_TEST)
-  glBindFramebuffer(GL_FRAMEBUFFER, RENDER_FRAMEBUFFER)
-  glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-  
-  render(screen)
-  yield() 
-  #Read all the selection queries
-  if !isempty(SELECTION_QUERIES)
-    glReadBuffer(GL_COLOR_ATTACHMENT1)
-    for (key, value) in SELECTION_QUERIES
-      if value.w < 1 || value.w > 5000
-        println(value.w) # debug output
-      end
-      if value.h < 1 || value.h > 5000
-        println(value.h) # debug output
-      end
-      const data = Array(Vector2{Uint16}, value.w, value.h)
-      glReadPixels(value.x, value.y, value.w, value.h, STENCIL_BUFFER.format, STENCIL_BUFFER.pixeltype, data)
-      push!(SELECTION[key], convert(Matrix{Vector2{Int}}, data))
+    global ROOT_SCREEN
+    while ROOT_SCREEN.inputs[:open].value
+        renderloop(ROOT_SCREEN)
     end
-  end
-  yield() 
-  glDisable(GL_SCISSOR_TEST)
-  glBindFramebuffer(GL_FRAMEBUFFER, 0)
-  glClear(GL_COLOR_BUFFER_BIT)
+    GLFW.Terminate()
+end
 
-  render(postprocess_robj)
-  GLFW.SwapBuffers(screen.nativewindow)
-  GLFW.PollEvents()
-  yield() 
-  
+
+function renderloop(screen)
+    yield() 
+    glDisable(GL_SCISSOR_TEST)
+    glBindFramebuffer(GL_FRAMEBUFFER, RENDER_FRAMEBUFFER)
+    glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    render(screen)
+    yield() 
+    #Read all the selection queries
+    if !isempty(SELECTION_QUERIES)
+        glReadBuffer(GL_COLOR_ATTACHMENT1)
+        for (key, value) in SELECTION_QUERIES
+            if value.w < 1 || value.w > 5000
+            println(value.w) # debug output
+            end
+            if value.h < 1 || value.h > 5000
+            println(value.h) # debug output
+            end
+            const data = Array(Vector2{Uint16}, value.w, value.h)
+            glReadPixels(value.x, value.y, value.w, value.h, STENCIL_BUFFER.format, STENCIL_BUFFER.pixeltype, data)
+            push!(SELECTION[key], convert(Matrix{Vector2{Int}}, data))
+        end
+    end
+    yield() 
+    glDisable(GL_SCISSOR_TEST)
+    glBindFramebuffer(GL_FRAMEBUFFER, 0)
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    render(postprocess_robj)
+    GLFW.SwapBuffers(screen.nativewindow)
+    GLFW.PollEvents()
+    yield() 
+
 end
 
 glClearColor(0, 0, 0, 0)
+
