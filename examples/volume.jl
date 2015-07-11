a00fdb8e574651ba010c58db1cb80f01a1429c1d
Simon Danisch <sdanisch@gmail.com>
2015-07-11 16:54:46 -0700

    started including volume example

---
+++ examples/volume.jl
@@ -0,0 +1,12 @@
+using GLVisualize, NPZ
+
+function volume_data(N)
+	volume 	= Float32[sin(x/15.0)+sin(y/15.0)+sin(z/15.0) for x=1:N, y=1:N, z=1:N]
+	max 	= maximum(volume)
+	min 	= minimum(volume)
+	volume 	= (volume .- min) ./ (max .- min)
+end
+
+
+const vol_data = volume_data(128)
+
