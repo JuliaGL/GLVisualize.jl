5c6abf5ab7a0d1f1004b5478677a2ff8f20f77cf
Sdanisch <sdanisch@gmail.com>
2015-08-18 22:52:22 +0200

    changes for new colortypes

--- src/visualize/surface.jl
+++ src/visualize/surface.jl
@@ -1,17 +1,17 @@
 Base.minimum(t::Texture) = minimum(gpu_data(t))
 Base.maximum(t::Texture) = maximum(gpu_data(t))
 
-function visualize_default(grid::Union(Texture{Float32, 2}, Matrix{Float32}), ::Style{:surface}, kw_args...) 
+function visualize_default(grid::Union(Texture{Float32, 2}, Matrix{Float32}), ::Style{:surface}, kw_args...)
     grid_min    = get(kw_args[1], :grid_min, Vec2f0(-1, -1))
     grid_max    = get(kw_args[1], :grid_max, Vec2f0( 1,  1))
     grid_length = grid_max - grid_min
     scale = Vec3f0((1f0 ./[size(grid)...])..., 1f0)
     Dict(
         :primitive  => GLMesh2D(Rectangle(0f0,0f0,1f0,1f0)),
-        :color      => RGBAU8[rgbaU8(1,0,0,1), rgbaU8(1,1,0,1), rgbaU8(0,1,0,1), rgbaU8(0,1,1,1), rgbaU8(0,0,1,1)],
+        :color      => RGBAU8[RGBAU8(1,0,0,1), RGBAU8(1,1,0,1), RGBAU8(0,1,0,1), RGBAU8(0,1,1,1), RGBAU8(0,0,1,1)],
         :grid_min   => grid_min,
         :grid_max   => grid_max,
-        :color_norm => Vec2f0(minimum(grid), maximum(grid)), 
+        :color_norm => Vec2f0(minimum(grid), maximum(grid)),
         :scale      => scale .* Vec3f0(grid_length..., 1f0)
     )
 end
@@ -47,7 +47,7 @@ function visualize{T <: Input{Matrix{Float32}}}(x::T, y::T, z::T, s::Style{:surf
     lift(update!, xt, x); lift(update!, yt, y); lift(update!, yt, y)
     visualize(xt, yt, zt, s, customizations)
 end
-visualize{T <: Matrix{Float32}}(x::T, y::T, z::T, s::Style{:surface}, customizations=visualize_defaults(z, s)) = 
+visualize{T <: Matrix{Float32}}(x::T, y::T, z::T, s::Style{:surface}, customizations=visualize_defaults(z, s)) =
     visualize(Texture(x),Texture(y), Texture(z), s, customizations)
 
 function visualize{T <: Texture{Float32, 2}}(x::T, y::T, z::T, s::Style{:surface}, customizations=visualize_defaults(z, s))
@@ -65,7 +65,7 @@ function visualize{T <: Texture{Float32, 2}}(x::T, y::T, z::T, s::Style{:surface
     bb = lift(AABB{Float32}, min_x, min_y, min_z, max_x, max_y, max_z) # This is not accurate as I'm not recalcuting the data when something updates
 
     program = assemble_instanced(
-        x, data, 
+        x, data,
         "util.vert", "surface2.vert", "standard.frag",
         boundingbox=bb
     )
