ad53714c737740d78e4ae967d05e61c7b1c4ebb1
Sdanisch <sdanisch@gmail.com>
2015-08-28 16:47:43 +0200

    using the compose interface more

--- src/visualize/containers.jl
+++ src/visualize/containers.jl
@@ -1,4 +1,4 @@
-visualize_default(::Array, ::Style, kw_args...) = Dict(
+visualize_default{T <: Composable, N}(::Array{T, N}, ::Style, kw_args...) = Dict(
     :gap 	=> Input(Vec3f0(0.1, 0.1, 0.0)),
    	:scale 	=> Vec3f0(1.0, 1.0, 1.0)
 )
@@ -9,7 +9,7 @@ function grid_translation(scale, model_scale, bb, model, i=1, j=1, k=1)
 	translationmatrix(Vec3f0(i-1, j-1, k-1).*scale)*scalematrix(model_scale*scale)*translationmatrix(-minimum(bb))*model
 end
 
-function visualize{N}(grid::Array{RenderObject, N}, s::Style, customizations=visualize_default(grid, s))
+function visualize{T <: Composable, N}(grid::Array{T, N}, s::Style, customizations=visualize_default(grid, s))
     @materialize! gap, model, scale = customizations
 	for ind=1:length(grid)
 		robj 	= grid[ind]
@@ -21,4 +21,27 @@ function visualize{N}(grid::Array{RenderObject, N}, s::Style, customizations=vis
 	vec(grid)
 end
 
+visualize_default{T <: Composable}(::Vector{T}, ::Style, kw_args...) = Dict(
+    :gap 	=> 0f0,
+)
+y_coord(x) = x[2]
+function list_translation(y_start, x_align, bb)
+	w = width(bb)[2]
+	x_move = x_align - minimum(bb)[1]
+	translationmatrix(Vec3f0(x_move, y_start-w, 0))
+end
 
+function visualize{T <: Composable}(list::Vector{T}, s::Style, customizations=visualize_default(grid, s))
+    @materialize! gap = customizations
+    elem 	= first(list)
+    bb_s 	= boundingbox(elem)
+	y_start = lift(-, lift(y_coord, lift(getfield, bb_s, :minimum)), gap)
+	x_align = lift(first, lift(minimum, bb_s))
+	for elem in list[2:end]
+		bb_s 		 = boundingbox(elem)
+		transformation(elem, lift(list_translation, y_start, x_align, bb_s))
+		y_width 	 = lift(y_coord, lift(width, bb_s))
+		y_start 	 = lift(-, y_start, lift(+, y_width, gap))
+	end
+	Context(list...)
+end
