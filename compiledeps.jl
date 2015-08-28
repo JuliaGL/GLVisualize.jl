c4048aaac842607d649acdfa70eba6f6b6f3fd82
Sdanisch <sdanisch@gmail.com>
2015-08-28 23:52:48 +0200

    not necessary with new precompile

--- compiledeps.jl
+++
@@ -1,7 +0,0 @@
-for m in [
-        :FixedSizeArrays, :Quaternions,
-        :VideoIO, :GLFW, :Reactive, :AbstractGPUArray,
-        :FreeTypeAbstraction,:ImageIO
-        ]
-    Base.compile(m)
-end
