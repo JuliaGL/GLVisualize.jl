e5b08427c3eb74863072a18f24597d7bb02cd809
SimonDanisch <sdanisch@gmail.com>
2016-01-31 15:14:29 +0100

    different colors for 3d lines

--- examples/contours.jl
+++ examples/contours.jl
@@ -1,4 +1,4 @@
-using Contour, GLVisualize, GeometryTypes, GLAbstraction
+using Contour, GLVisualize, GeometryTypes, GLAbstraction, Colors
 xrange = -5f0:0.02f0:5f0
 yrange = -5f0:0.02f0:5f0
 
@@ -6,15 +6,19 @@ z = Float32[sin(1.3*x)*cos(0.9*y)+cos(.8*x)*sin(1.9*y)+cos(y*.2*x) for x in xran
 mini = minimum(z)
 maxi = maximum(z)
 w = glscreen()
-
+colormap = map(x->RGBA{Float32}(x, 1.0), colormap("Blues"))
 trans = Vec3f0(0)
+toindex(val, mini, maxi, len) = floor(Int, (((val-mini)/(maxi-mini))*(length(colormap)-1)))+1
 for h in mini:0.2f0:maxi
     c = contour(xrange, yrange, z, h)
     for elem in c.lines
         points = map(elem.vertices) do p
             Point3f0(p, h)
         end
-        view(visualize(points, :lines), method=:perspective)
+        view(visualize(
+            points, :lines,
+            color=colormap[toindex(h, mini, maxi, length(colormap))]
+        ), method=:perspective)
         view(visualize(elem.vertices, :lines, model=translationmatrix(Vec3f0(0,11,0))), method=:perspective)
     end
 end
