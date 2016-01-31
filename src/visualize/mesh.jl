58923d1bea0a7e2ed01a25755448cf3e80db9233
SimonDanisch <sdanisch@gmail.com>
2016-02-01 00:17:59 +0100

    make meshes work with signals

--- src/visualize/mesh.jl
+++ src/visualize/mesh.jl
@@ -1,24 +1,24 @@
-_default(mesh::GLNormalAttributeMesh, s::Style, data::Dict) = @gen_defaults! data begin
+_default{M<:GLNormalAttributeMesh}(mesh::TOrSignal{M}, s::Style, data::Dict) = @gen_defaults! data begin
     main 		= mesh
-    boundingbox = GLBoundingBox(mesh)
+    boundingbox = const_lift(GLBoundingBox, mesh)
     shader 		= GLVisualizeShader("util.vert", "attribute_mesh.vert", "standard.frag")
 end
 
-_default(mesh::GLNormalMesh, s::Style, data::Dict) = @gen_defaults! data begin
+_default{M<:GLNormalMesh}(mesh::TOrSignal{M}, s::Style, data::Dict) = @gen_defaults! data begin
     main 		= mesh
     color 		= default(RGBA{Float32}, s)
-    boundingbox = GLBoundingBox(mesh)
+    boundingbox = const_lift(GLBoundingBox, mesh)
     shader 		= GLVisualizeShader("util.vert", "standard.vert", "standard.frag")
 end
 
-_default(main::GLPlainMesh, ::style"grid", data::Dict) = @gen_defaults! data begin
+_default{M<:GLPlainMesh}(main::TOrSignal{M}, ::style"grid", data::Dict) = @gen_defaults! data begin
     primitive       = main
     color           = default(RGBA, s, 1)
     bg_colorc       = default(RGBA, s, 2)
     grid_thickness  = Vec3f0(2)
     gridsteps       = Vec3f0(5)
+    boundingbox     = const_lift(GLBoundingBox, mesh)
     shader          = GLVisualizeShader("grid.vert", "grid.frag")
-    boundingbox     = GLBoundingBox(primitive)
 end
 #=
 empty!(robj.prerenderfunctions)
