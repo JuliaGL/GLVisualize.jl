7c32224015a5c7a3add396334a554857f215f29d
SimonDanisch <sdanisch@gmail.com>
2016-02-22 23:01:13 +0100

    removed failing examples

--- examples/interactive/color_edit.jl
+++
@@ -1,9 +0,0 @@
-using GLVisualize, Colors, ModernGL, GeometryTypes, GLAbstraction, GLWindow, FileIO
-w = glscreen()
-v, colortex = vizzedit(map(RGBA{U8}, colormap("blues", 7)), w)
-
-robj = visualize(rand(Float32, 32,32), color_map=colortex, color_norn=Vec2f0(0,2))
-view(robj, w)
-view(v, w, camera=:fixed_pixel)
-
-renderloop(w)
