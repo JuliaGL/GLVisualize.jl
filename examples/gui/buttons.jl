421d73b65e770ba339f177976d98d147e3c0e019
SimonDanisch <sdanisch@gmail.com>
2016-11-09 15:22:21 +0100

    refactor edit menu

--- examples/gui/buttons.jl
+++ examples/gui/buttons.jl
@@ -30,54 +30,55 @@ function lorenz(array::Vector, a = 5.0 ,b = 2.0, c = 6.0, d = 0.01)
     array
 end
 
-function main(window)
-    primitives = GLNormalMesh[
-        loadasset("cat.obj"),
-        Sphere(Point3f0(0), 1f0),
-        Pyramid(Point3f0(0), 1f0, 1f0),
-        HyperRectangle(Vec3f0(0), Vec3f0(1))
-    ]
+primitives = GLNormalMesh[
+    loadasset("cat.obj"),
+    Sphere(Point3f0(0), 1f0),
+    Pyramid(Point3f0(0), 1f0, 1f0),
+    HyperRectangle(Vec3f0(0), Vec3f0(1))
+]
 
-    w = Dict(
-        :colora => RGBA(0.7f0, 1f0, 0.5f0, 1f0),
-        :colorb => RGBA(1f0, 0f0, 0f0, 1f0),
-        :scale => 0.4,
-        :a => 24f0,
-        :b => 10f0,
-        :c => 6.0f0,
-        :d => 0.01f0,
-        :colornorm => Vec2f0(0, 1.2),
-        :primitive => primitives
-    )
-    editarea, viewarea = x_partition_abs(window.area, 60mm)
-    editscreen = Screen(
-        window, area = editarea,
-        color = RGBA{Float32}(0.98, 0.98, 1, 1)
-    )
-    viewscreen = Screen(window, area = viewarea)
-    GLVisualize.extract_edit_menu(w, editscreen, true)
+w = [
+    :a => 24f0,
+    :b => 10f0,
+    :c => 6.0f0,
+    :d => 0.01f0,
+    :scale => 0.4,
 
-    n1, n2 = 18, 30
-    N = n1*n2
+    :colora => RGBA(0.7f0, 1f0, 0.5f0, 1f0),
+    :colorb => RGBA(1f0, 0f0, 0f0, 1f0),
+    :colornorm => Vec2f0(0, 1.2),
 
-    args = map(value, (w[:a], w[:b], w[:c], w[:d]))
-    v0 = lorenz(zeros(Point3f0, N), args...)
-    positions = foldp(lorenz, v0, w[:a], w[:b], w[:c], w[:d])
-    scales = const_lift(Vec3f0, w[:scale])
-    rotations = map(diff, positions)
-    rotations = map(x-> push!(x, x[end]), rotations)
-    cmap = map((a,b)->[a,b], w[:colora], w[:colorb])
+    :primitive => primitives
+]
 
-    _view(visualize(
-        (w[:primitive], positions),
-        scale = scales, rotation = rotations,
-        color_map = cmap, color_norm = w[:colornorm]
-    ), viewscreen)
+editarea, viewarea = x_partition_abs(window.area, 60mm)
+editscreen = Screen(
+    window, area = editarea,
+    color = RGBA{Float32}(0.98, 0.98, 1, 1)
+)
+viewscreen = Screen(window, area = viewarea)
+menu, w = GLVisualize.extract_edit_menu(w, editscreen, true)
+_view(menu, editscreen, camera = :fixed_pixel)
 
-    center!(viewscreen)
-end
+n1, n2 = 18, 30
+N = n1*n2
 
-main(window)
+args = map(value, (w[:a], w[:b], w[:c], w[:d]))
+v0 = lorenz(zeros(Point3f0, N), args...)
+positions = foldp(lorenz, v0, w[:a], w[:b], w[:c], w[:d])
+scales = const_lift(Vec3f0, w[:scale])
+rotations = map(diff, positions)
+rotations = map(x-> push!(x, x[end]), rotations)
+cmap = map((a,b)->[a,b], w[:colora], w[:colorb])
+
+_view(visualize(
+    (w[:primitive], positions),
+    scale = scales, rotation = rotations,
+    color_map = cmap, color_norm = w[:colornorm]
+), viewscreen)
+
+center!(viewscreen)
+
 
 
 if !isdefined(:runtests)
--- src/gui/container.jl
+++ src/gui/container.jl
@@ -6,26 +6,28 @@ function extract_edit_menu(robj::RenderObject, edit_screen, isvisible)
     extract_edit_menu(data, edit_screen, isvisible)
 end
 
-makesignal2(s::Signal) = s
 makesignal2(v) = Signal(v)
+makesignal2(s::Signal) = s
 makesignal2(v::GPUArray) = v
+keytype{K, T}(::Dict{K, T}) = K
+keytype{K, T}(::AbstractVector{Pair{K, T}}) = K
+keytype{K}(::AbstractVector{Pair{K}}) = K
 
-function extract_edit_menu(
-        edit_dict::Dict, edit_screen, isvisible;
-        mm = 3.71f0, filter_fun= (k, v_v) -> true,
-        icon_size=32, knob_scale=1.6mm
+
+
+function extract_edit_menu{T <: Pair}(
+        edit_dict::Union{AbstractVector{T}, Dict}, edit_screen, isvisible;
+        mm = 3.71f0, filter_fun = (k, v_v) -> true,
+        icon_size = 32, knob_scale = 1.6mm
     )
-    lines = Point2f0[]
 
     screen_w = widths(edit_screen)[1] - round(Int, 10mm)
-
-    labels = String[]
     pos = 1mm
     widget_text = 4mm
-    atlas = GLVisualize.get_texture_atlas()
-    font = GLVisualize.defaultfont()
-    textpositions = Point2f0[]
-    for (k,v) in edit_dict
+    visses = Pair{String, Context{GLAbstraction.DeviceUnit}}[]
+    K = keytype(edit_dict)
+    signal_dict = Dict{K, Signal}()
+    for (k, v) in edit_dict
         filter_fun(k, v) || continue
         label = string(k)
         s = makesignal2(v)
@@ -36,34 +38,9 @@ function extract_edit_menu(
                 area = (screen_w, icon_size),
                 knob_scale = knob_scale
             )
-            edit_dict[k] = sig
-            bb = value(boundingbox(vis))
-            height = widths(bb)[2]
-            mini = minimum(bb)
-            to_origin = -Vec3f0(mini[1], mini[2], 0)
-            GLAbstraction.transform!(vis, translationmatrix(Vec3f0(2mm,pos,0)+to_origin))
-            _view(vis, edit_screen, camera=:fixed_pixel)
-            pos += round(Int, height) + 1mm
-
-            push!(labels, label)
-            append!(textpositions,
-                GLVisualize.calc_position(label, Point2f0(1mm, pos), widget_text, font, atlas)
-            )
-            pos += widget_text + 4mm
-            push!(lines, Point2f0(0, pos-2mm), Point2f0(screen_w, pos-2mm))
-
+            push!(visses, label => vis)
+            signal_dict[k] = sig
         end
     end
-    _view(visualize(
-        join(labels), position = textpositions,
-        color = RGBA{Float32}(0.8, 0.8, 0.8, 1.0),
-        relative_scale = widget_text
-    ), edit_screen, camera=:fixed_pixel)
-
-    _view(visualize(
-        lines, :linesegment, thickness = 0.25mm,
-        color = RGBA{Float32}(0.9, 0.9, 0.9, 1.0)
-    ), edit_screen, camera=:fixed_pixel)
-
-    pos
+    visualize(visses), signal_dict
 end
