8b196d14e3a303420f1defa622e3c58bd3546a26
SimonDanisch <sdanisch@gmail.com>
2016-11-17 12:12:20 +0100

    make example a bit nicer

--- examples/lines/lines3D.jl
+++ examples/lines/lines3D.jl
@@ -16,17 +16,13 @@ TL = linspace(-2f0 * pi, 2f0 * pi, n)
 # We create a list of positions and connections, each describing a line.
 # We will collapse them in one array before plotting.
 xyz    = Point3f0[]
-colors = RGBA{Float32}[]
+intensities = Float32[]
 
-# creates some distinguishable colors from which we can sample for each line
-base_colors1 = distinguishable_colors(nloops, RGB{Float64}(1,0,0))
+
 # Create each line one after the other in a loop
-for i=1:nloops
+for i = 1:nloops
     append!(xyz, [Point3f0(sin(t), cos((2 + .02 * i) * t), cos((3 + .03 * i) * t)) for t in TL])
-    unique_colors = base_colors1[i]
-    hsv = HSV(unique_colors)
-    color_palette = map(x->RGBA{Float32}(x, 1.0), sequential_palette(hsv.h, n, s=hsv.s))
-    append!(colors, color_palette)
+    append!(intensities, (i for t in TL))
 end
 
 # map comes from Reactive.jl and allows you to map any Signal to another.
@@ -36,7 +32,17 @@ rotation = map(timesignal) do t
     rotationmatrix_z(Float32(t*2pi)) # -> 4x4 Float32 rotation matrix
 end
 
-lines3d = visualize(xyz, :lines, color=colors, model=rotation)
+# creates a color map from which we can sample for each line
+# and add some transparency
+cmap = map(x-> RGBA{Float32}(x, 0.4), colormap("Blues", nloops))
+
+lines3d = visualize(
+    xyz, :lines,
+    intensity = intensities,
+    color_map = cmap,
+    color_norm = Vec2f0(0, nloops), # normalize intensities. Lookup in cmap will be between 0-1
+    model = rotation
+)
 
 _view(lines3d, window, camera=:perspective)
 
