5aebc66058984186b5e19c22a5dfe51fe006b915
SimonDanisch <sdanisch@gmail.com>
2016-11-25 13:35:46 +0100

    make it easier to restart parallel

--- src/parallel.jl
+++ src/parallel.jl
@@ -22,14 +22,15 @@ using FreeType
 # on which we run GLVisualize
 # I will move these functions to GLVisualize once this is tested better!
 function set_arg!(obj, key, value)
-    @spawnat workerid begin
+    @spawnat workerid[] begin
         GLAbstraction.set_arg!(fetch(obj), key, value)
+        #Reactive.post_empty()
         nothing
     end
 end
 
 function _view(main, style=:default, cam=:perspective; kw_args...)
-    @spawnat workerid begin
+    @spawnat workerid[] begin
         obj = GLVisualize.visualize(main, style; kw_args...)
         GLVisualize._view(obj, camera=cam)
         obj
@@ -37,26 +38,30 @@ function _view(main, style=:default, cam=:perspective; kw_args...)
 end
 
 function is_current_open()
-    remotecall_fetch(workerid) do
+    remotecall_fetch(workerid[]) do
         isempty(GLVisualize.get_screens()) && return false
         isopen(GLVisualize.current_screen())
     end
 end
 
 function empty_current!()
-    remotecall(workerid) do
+    remotecall(workerid[]) do
         empty!(GLVisualize.current_screen())
         nothing
     end
 end
 
+global const workerid = Ref(0)
+
 function glscreen(
         id = workers()[end];
         color = RGBA(1f0, 1f0, 1f0, 1f0)
     )
-    global const workerid = id
+    if workerid[] == 0 || id != workers()[end]
+        workerid[] = id
+    end
     # start GLVisualize worker process
-    @spawnat workerid begin
+    @spawnat workerid[] begin
         window = GLVisualize.glscreen(color = color)
         @async GLWindow.waiting_renderloop(window)
         nothing
