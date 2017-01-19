888e54870f3b26bc4f222d6a96583ac1c91c9453
SimonDanisch <sdanisch@gmail.com>
2017-01-19 17:38:09 +0100

    add video streaming

--- src/videotool.jl
+++ src/videotool.jl
@@ -45,7 +45,7 @@ function create_video(frames::Vector, name, screencap_folder, resample_steps=0, 
     end
 end
 
-function create_video(frame, name, path, resample_steps=0)
+function create_video(frame, name, path, resample_steps = 0)
     println("saving static image for $name")
     targetpath = abspath(joinpath(path, "$(name).png"))
     for i=1:resample_steps
@@ -57,3 +57,27 @@ end
 #`ffmpeg -i volume.webm -codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 4 -vf scale=-1:480 -codec:a libvorbis -b:a 128k volume2.webm`
 # ffmpeg version
 # ffmpeg -r 24 -pattern_type glob -i '*.png' -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4
+
+function create_video_stream(path, window, framerate = 30)
+    #codec = `-codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 8`
+    tex = GLWindow.framebuffer(window).color
+    res = size(tex)
+    io, process = open(`ffmpeg -f rawvideo -pixel_format rgb24 -s:v $(res[1])x$(res[2]) -r $framerate -i pipe:0 -vf vflip -y $path`, "w")
+    io, Array(RGB{U8}, res) # tmp buffer
+end
+function add_frame!(io, window, buffer)
+    #codec = `-codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 8`
+    tex = GLWindow.framebuffer(window).color
+
+    write(io, map(RGB{U8}, gpu_data(tex)))
+end
+
+function webm_batchconvert(oldpath, newpath, scale = 0.5)
+    for file in readdir(oldpath)
+        isfile(file) || continue
+        f = joinpath(oldpath, file)
+        name, ext = splitext(file)
+        out = joinpath(newpath, name * ".webm")
+        run(`ffmpeg -i $f -c:v libvpx-vp9 -threads 16 -b:v 2000k -c:a libvorbis -threads 16 -vf scale=iw*$scale:ih*$scale $out`)
+    end
+end
