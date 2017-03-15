4b793bcd412dc634a218fe29d07a0b9ff7a4a180
SimonDanisch <sdanisch@gmail.com>
2017-03-15 20:41:36 +0100

    don't fail if optional ffmpeg is not installed

--- examples/introduction/video_recording.jl
+++ examples/introduction/video_recording.jl
@@ -6,6 +6,7 @@ end
 description = """
 Example of how to record a video from GLVisualize
 """
+
 kitty = visualize(loadasset("cat.obj"))
 _view(kitty, window)
 
@@ -20,23 +21,27 @@ while true # for some reason, folder retured by mktempdir isn't usable -.-
     name = path * "/$(randstring()).mkv"
     isfile(name) || break
 end
+# only try recording when ffmpeg is installed
+if success(`ffmpeg -h`)
+    # create a stream to which we can add frames
+    io, buffer = GLVisualize.create_video_stream(name, window)
+    for i=1:10 # record 10 frames
+        # do something
+        GLAbstraction.set_arg!(kitty, :color, RGBA{Float32}(1, 0, 1-(i/10), i/10))
+        #render current frame
+        # if you call @async renderloop(window) you can replace this part with yield
+        GLWindow.render_frame(window)
+        GLWindow.swapbuffers(window)
+        GLWindow.poll_reactive()
 
-# create a stream to which we can add frames
-io, buffer = GLVisualize.create_video_stream(name, window)
-for i=1:10 # record 10 frames
-    # do something
-    GLAbstraction.set_arg!(kitty, :color, RGBA{Float32}(1, 0, 1-(i/10), i/10))
-    #render current frame
-    # if you call @async renderloop(window) you can replace this part with yield
-    GLWindow.render_frame(window)
-    GLWindow.swapbuffers(window)
-    GLWindow.poll_reactive()
-
-    # add the frame from the current window
-    GLVisualize.add_frame!(io, window, buffer)
+        # add the frame from the current window
+        GLVisualize.add_frame!(io, window, buffer)
+    end
+    # closing the stream will trigger writing the video!
+    close(io)
+else
+    info("skipped ffmpged video recording, since ffmpeg is not installed!")
 end
-# closing the stream will trigger writing the video!
-close(io)
 
 if !isdefined(:runtests)
     renderloop(window)
