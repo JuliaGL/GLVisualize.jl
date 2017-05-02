8b8da6b8098d88a9c8883f1a088a0487b8dd1c10
SimonDanisch <sdanisch@gmail.com>
2017-05-02 13:13:35 +0200

    remove checkout code

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,6 @@ end
 
 if isheadless()
     # need this branch for better coverage report!
-    cd(Pkg.dir("GLAbstraction")) do
-        run(`git fetch origin`)
-        run(`git checkout master`)
-    end
     include("test_static.jl")
 else
     include("test_interactive.jl")
