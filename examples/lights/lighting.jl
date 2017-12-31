c63be23f903be4c27c6314c755403258d2d79b4d
eoudet <edouard.oudet@imag.fr>
2017-12-31 12:11:19 +0100

    --message=redefined default values + example

--- examples/lights/lighting.jl
+++ examples/lights/lighting.jl
@@ -1,14 +1,16 @@
 using GLVisualize, GeometryTypes, GLAbstraction, Colors, FileIO
 
 window = GLVisualize.glscreen(); δ = 0.45
+ambientcolordefault = Vec3f0(0.1)
+materialdefault = Vec4f0(0.9, 0.7, 0.3, 0.9) # (ambient, diffuse, specular, specularcolorcoeff) ∈ [0, 1]
+shininessdefault = Float32(5.)
 lightd = Vec3f0[Vec3f0(1.0, 1.0, 1.0), Vec3f0(0.1, 0.1, 0.1),
-                Vec3f0(0.9, 0.9, 0.9), Vec3f0(10, 50, 200)]
+                Vec3f0(0.9, 0.9, 0.9), Vec3f0(20, 0, 20)]
 markerm = :o
-#markerm = :□
 for x = 0:2
     for y = 0:2
-        dc = linspace(0, 1., 3)[x + 1]
-        sc = linspace(0, .6, 3)[2 - y + 1]
+        dc = linspace(0, 1, 3)[x + 1] * materialdefault[2]
+        sc = linspace(0, 1, 3)[3 - y] * materialdefault[3]
         lmesh = if markerm == :o
             GLNormalMesh(Sphere{Float32}(Point3f0(x, y, 0), Float32(δ)), 50)
         elseif markerm == :□
@@ -17,13 +19,12 @@ for x = 0:2
         end
         material = Vec4f0(1, dc, sc, 1.)
         GLVisualize._view(GLVisualize.visualize(lmesh, color = RGBA{Float32}(1, 0, 0, 1.),
-                                                light = lightd, shininess = Float32(8.),
+                                                light = lightd, shininess = shininessdefault,
                                                 material = material,
                                                 ambientcolor = Vec3f0(0.01)), window)
     end
 end
 lighting = GLVisualize.Lighting([lightd[end],])
-cd = 1.
 for x = 0:2
     for y = 0:2
         ac = if x == 0
@@ -33,8 +34,7 @@ for x = 0:2
         elseif x == 2
             ones(3)
         end
-        ac /= cd
-        as = linspace(0, 0.5, 3)[y + 1]
+        as = linspace(0, 1, 3)[y + 1] * materialdefault[1]
         lmesh = if markerm == :o
             GLNormalMesh(Sphere{Float32}(Point3f0(x , 2 - y - 4, 0), Float32(δ)), 50)
         elseif markerm == :□
@@ -44,7 +44,7 @@ for x = 0:2
         material = Vec4f0(as, 0.4, 0.8, 1.)
         GLVisualize._view(GLVisualize.visualize(lmesh, color = RGBA{Float32}(1, 0, 0, 1.),
                                                 ambientcolor = Vec3f0(ac...),
-                                                lighting = lighting, shininess = Float32(8.),
+                                                lighting = lighting, shininess = shininessdefault,
                                                 material = material), window)
     end
 end
